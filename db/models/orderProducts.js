1ed0c7377ed2e5a78109c761e87d17f2c27a16fe
Surabhi Nigam <surabhi@Surabhis-MBP.fios-router.home>
2017-01-12 23:20:25 -0500

    adding price and quantity to the join table

--- db/models/orderProducts.js
+++ db/models/orderProducts.js
@@ -5,7 +5,15 @@ const db = require('APP/db');
 
 // This table is used as the JOIN table for Order and Product
 const OrderProducts = db.define('orderProducts', {
-	quantity: Sequelize.INTEGER // quantity of each item/cart combo
+	quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0 //"Out of stock"/In stock will be handled on frontend
+  },
+	price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+    defaultValue: 0
+  }
 });
 
 module.exports = OrderProducts;
