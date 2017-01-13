b4eec9192a8a9e645f8553cec8ad98182b354374
Yoo-Nah Park <ypark2270@gmail.com>
2017-01-13 16:33:28 -0500

    deleted Cart and Payment models and associations in index.js

--- db/models/index.js
+++ db/models/index.js
@@ -7,15 +7,12 @@
 // Require models
 const User = require('./user');
 const Address = require('./address');
-const Cart = require('./cart');
 const Order = require('./order');
-const Payment = require('./payment');
 const Product = require('./product');
 const Review = require('./review');
 const Category = require('./category');
 
 // Require JOIN table models
-const CartProducts = require('./cartProducts');
 const OrderProducts = require('./orderProducts');
 
 // # Add associations
@@ -26,42 +23,21 @@ Product.hasMany(Review);
 // User foreign key on each Review
 Review.belongsTo(User, {as: 'Author'});
 
-//took the line below out, because we think it might not be necessary
-//it was adding a user_id field to the reviews table when we already had author_id
-
-//User.hasMany(Review, {as: 'Author'});
-
 // User foreign key on each Address
 User.hasMany(Address);
 
 // Category foreign key on each Product
 Category.hasMany(Product);
 
-// User foreign key on each payment
-Payment.belongsTo(User);
-
-// Address foreign key as 'BillingAddress' on each payment
-Payment.belongsTo(Address, {as: 'BillingAddress'});
-
 // Address foreign key as 'ShippingAddress' on each Order
 Order.belongsTo(Address, {as: 'ShippingAddress'});
 
-// Payment foreign key on each Order
-Order.belongsTo(Payment);
-
 // User foreign key on each Order
 Order.belongsTo(User);
-
-// User foreign key on each Cart
-Cart.belongsTo(User);
-
-// Join table 'CartProduct'
-Cart.belongsToMany(Product, {through: CartProducts});
-Product.belongsToMany(Cart, {through: CartProducts});
 
 // Join table 'OrderProduct'
 Order.belongsToMany(Product, {through: OrderProducts});
 Product.belongsToMany(Order, {through: OrderProducts});
 
 
-module.exports = {User, Product, Review, Category, Address, Cart, Order, Payment}
+module.exports = {User, Product, Review, Category, Address, Order}
--- db/models/order.js
+++ db/models/order.js
@@ -7,7 +7,7 @@ const Order = db.define('orders', {
 	status: {
 		type: Sequelize.ENUM,
 		allowNull: false,
-		values: ['created', 'processing', 'canceled', 'shipped', 'delivered', 'completed']
+		values: ['cart', 'created', 'processing', 'canceled', 'shipped', 'delivered', 'completed']
 	},
 	total: {
 		type: Sequelize.FLOAT,
