897a57e9f5b4acf47a859063430150a4925f05fd
Andrew Garcia <runandrew@gmail.com>
2017-01-15 15:50:03 -0500

    fixing a seeding error

--- db/seed.js
+++ db/seed.js
@@ -113,9 +113,9 @@ function seedFn(seedData, model) {
 // Sync the seed data
 db.didSync
   .then(() => db.sync({force: true}))
-  .then(seedFn(usersToAdd, 'category'))
+  .then(seedFn(usersToAdd, 'users'))
   .then(users => console.log(`Seeded ${users.length} users OK`))
-  .then(seedFn(categoriesToAdd, 'users'))
+  .then(seedFn(categoriesToAdd, 'category'))
   .then(categories => console.log(`Seeded ${categories.length} categories OK`))
   .then(seedFn(productsToAdd, 'products'))
   .then(products => console.log(`Seeded ${products.length} products OK`))
