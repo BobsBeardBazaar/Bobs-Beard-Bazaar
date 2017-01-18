079631722f33cccbbcbbf593d41852514fc5a9d4
Yoo-Nah Park <ypark2270@gmail.com>
2017-01-18 11:28:05 -0500

    return createError util function to module.exports

--- server/utils.js
+++ server/utils.js
@@ -1,9 +1,11 @@
 
 // Helper function, creates a new Error instance
-export const createError = (status, message) => {
+const createError = (status, message) => {
     const err = new Error();
     err.message = message;
     err.status = status;
     return err;
 }
+
+module.exports = { createError };
 
